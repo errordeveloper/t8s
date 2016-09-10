f01017db631a4315c3156ab5a7abdb228b3d0f09
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-09-10 08:52:59 +0100

    Move things around, allow multiple clusters, and master-only

---
+++ gce-testing/create-only-master.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -ex
+
+# Copyright 2016 The Kubernetes Authors All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+i="${1:-"1"}"
+
+gcloud compute networks create "kube-net-${i}" \
+  --mode 'auto'
+
+gcloud compute firewall-rules create "kube-extfw-${i}" \
+  --network "kube-net-${i}" \
+  --allow 'tcp:22,tcp:4040' \
+  --target-tags "kube-ext-${i}" \
+  --description 'External access only for SSH'
+
+gcloud compute firewall-rules create "kube-intfw-${i}" \
+  --network "kube-net-${i}" \
+  --allow 'tcp:443,tcp:9898,tcp:6783,udp:6783-6784' \
+  --source-tags "kube-int-${i}" \
+  --target-tags "kube-int-${i}" \
+  --description 'Internal access for the API & Weave Net ports'
+
+gcloud compute instance-groups unmanaged create "kube-master-group-${i}"
+
+common_instace_flags=(
+  --network kube-net-${i}
+  --image-family centos-7
+  --image-project centos-cloud
+  --metadata-from-file startup-script=provision.sh
+  --boot-disk-type pd-standard
+)
+
+gcloud compute instances create "kube-master-${i}-0" \
+  "${common_instace_flags[@]}" \
+  --tags "kube-int-${i},kube-ext-${i}" \
+  --boot-disk-size '10GB' \
+  --can-ip-forward \
+  --scopes 'storage-ro,compute-rw,monitoring,logging-write'
